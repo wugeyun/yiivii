8ccdf30fa5c6b41e2a621376effb352371ef939d
wuge <hswddan@qq.com>
2018-12-23 14:36:10 +0800

    add member module

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,15 +1,12 @@
 <?php
 namespace app\index\controller;
 
-class Index
-{
-    public function index()
-    {
+class Index {
+    public function index() {
         return view('');
     }
 
-    public function hello($name = 'ThinkPHP5')
-    {
+    public function hello($name = 'ThinkPHP5') {
         return 'hello,' . $name . '.';
     }
 }
---
+++ application/index/controller/Member.php
@@ -0,0 +1,8 @@
+<?php
+namespace app\index\controller;
+
+class Member {
+    public function index($name = '') {
+        return view('');
+    }
+}
