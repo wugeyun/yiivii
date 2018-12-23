11679a23762f6be6e94fb374de47a7e5b76da9d4
wuge <hswddan@qq.com>
2018-12-23 18:32:46 +0800

    update index.php

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,11 @@
 <?php
 namespace app\index\controller;
-
 class Index {
     public function index() {
         return view('');
     }
 
-    public function hello($name = 'ThinkPHP5') {
-        return 'hello,' . $name . '.';
+    public function howtouse() {
+        return view('');
     }
 }
