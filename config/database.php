c5d49f059eb1f37ba0d417cef3b4bbd698ab98ad
wuge <hswddan@qq.com>
2018-12-28 11:39:52 +0800

    set config

--- config/database.php
+++ config/database.php
@@ -19,7 +19,7 @@ return [
     // 用户名
     'username'        => 'yiivii',
     // 密码
-    'password'        => 'LkSPi4sdXCRfxmce',
+    'password'        => '',
     // 端口
     'hostport'        => '',
     // 连接dsn
