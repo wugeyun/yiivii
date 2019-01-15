0418cd5a5dcb9e1b3b2df9515abcca493f84168f
imas3410 <hswddan@qq.com>
2019-01-15 14:04:26 +0800

    login

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -4,6 +4,9 @@ use think\Controller;
 use think\Validate;
 use app\common\model\Member;
 class Login extends Controller {
+    /**
+     * @return mixed
+     */
     public function index() {
         return view('');
     }
@@ -16,36 +19,42 @@ class Login extends Controller {
         $data['code'] = 500;
         $email = trim($email);
         if($email){
-            //验证数据
+            //验证邮箱
             $validate = Validate::make([
                 'email' => 'email'
             ]);
-            if ($validate->check(['email'=>$email])) {
-                $data['code'] = 200;
-                //开始查询数据库
-                $user = Member::get(['email' => $email]);
-                if(empty($user)){
-                    $user = Member::create(['email' => $email]);
-                }
-                //判断生成登录码
-                for($i=1;$i<100;$i++){
+            switch ($validate->check(['email'=>$email])){
+                case true :
+                    //开始查询数据库
+                    $user = Member::get(['email' => $email]);
+                    if($user == null){
+                        $user = Member::create(['email' => $email]);
+                    }
                     $salt = rand(1111,9999);
                     $wait = time() - $user->lastget;
-                    if($wait > 600){
-                        Member::where('email',$email)->update(['salt'=>$salt,'lastget'=>time()]);
-                        $wait = 600;
-                        $mail_content = '<p>登 录 码【'.$salt.'】</p>';
-                        break;
-                    }else{
-                        $wait = 600 - $wait;
-                        $mail_content = '<p>登 录 码【'.$user->salt.'】</p>';
+                    switch ($wait){
+                        case ($wait > 600):
+                            $user->salt = $salt;
+                            $user->lastget = time();
+                            $user->save();
+                            $wait = 600;
+                            $mail_content = '<p>登 录 码【'.$salt.'】</p>';
+                            break;
+                        default:
+                            $wait = 600 - $wait;
+                            $mail_content = '<p>登 录 码【'.$user->salt.'】</p>';
+                            break;
                     }
-                }
-                //邮件发送登录码
-                $mail_content .= '<p>有效时间【'.$wait.' 秒】</p>';
-                $mail_content .= '<p>有效期至【'.date('Y-m-d H:i:s',time() + $wait).'】</p>';
-                $this_content = setEmailContent($mail_content);
-                sendMail($this_content,$email);
+                    $data['code'] = 200;
+                    //邮件发送登录码
+                    $mail_content .= '<p>有效时间【'.$wait.' 秒】</p>';
+                    $mail_content .= '<p>有效期至【'.date('Y-m-d H:i:s',time() + $wait).'】</p>';
+                    $this_content = setEmailContent($mail_content);
+                    sendMail($this_content,$email);
+                    break;
+                default:
+                    $data['code'] = 500;
+                    break;
             }
         }
         return json($data);
@@ -63,7 +72,7 @@ class Login extends Controller {
             $info = Member::where('salt',$salt)
             ->where('lastget','>',$time)
             ->find();
-            if(!empty($info)){
+            if($info != null){
                 //写入cookie
                 cookie('email',$info['email'],3600*24*90);
                 //清空
@@ -73,7 +82,7 @@ class Login extends Controller {
                 $info->save();
                 $data['code'] = 200;
             }else{
-                $data['code'] = 300;
+                $data['code'] = 500;
             }
         }
         return json($data);
