7df57ca96c0302225442861a5f8db95258432b12
imas3410 <hswddan@qq.com>
2019-01-16 09:09:03 +0800

    login member

--- application/index/controller/Member.php
+++ application/index/controller/Member.php
@@ -5,6 +5,11 @@ use app\common\controller\Common;
 use app\common\model\Order;
 use app\common\model\Member as MemberModel;
 class Member extends Common {
+    protected function initialize(){
+        if(session('?uid') == false){
+            $this->error('请使用Email登陆');
+        }
+    }
     /**
      * @return mixed
      */
