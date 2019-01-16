987741d5098e300ed188bfcda71b4a1eacb6ff4d
imas3410 <hswddan@qq.com>
2019-01-16 10:21:06 +0800

    dell nikename

--- application/common/controller/Common.php
+++ application/common/controller/Common.php
@@ -8,7 +8,6 @@ class Common extends Controller {
          * 系统状态初始化
          * 1000 游客状态
          * 1001 登陆状态
-         * 1002 nikename为空
          */
         //读取cookie
         $email = cookie('email');
@@ -16,7 +15,6 @@ class Common extends Controller {
             $user = Member::get(['email'=>$email]);
             session('uid',$user->id);
             session('email',$email);
-            $user['nikename'] == '' ? session('status',1002) : session('status',1001);
         }else{
             cookie('email',null);
         }
--- application/index/controller/Member.php
+++ application/index/controller/Member.php
@@ -16,16 +16,6 @@ class Member extends Common {
     public function index() {
         return view('');
     }
-    /**
-     * @param $nikename
-     */
-    public function setuser($nikename){
-        if(session('status') == 1002){
-            MemberModel::where('id',session('uid'))->update(['nikename' => $nikename]);
-            session('nikename',$nikename);
-            session('status',1001);
-        }
-    }
     public function order(){
 
     }
