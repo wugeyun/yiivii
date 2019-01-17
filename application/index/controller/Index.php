828df47a560b89949102f23ccc329a5f3e050211
imas3410 <hswddan@qq.com>
2019-01-17 16:40:07 +0800

    order list

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,12 +1,21 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use app\common\model\Order;
 use app\common\controller\Common;
 class Index extends Common {
+    /**
+     * @return mixed
+     */
     public function index() {
-        return view('');
+        $list = Order::limit(8)->select();
+        $data['list'] = $list;
+        return view('',$data);
     }
 
+    /**
+     * @return mixed
+     */
     public function howtouse() {
         return view('');
     }
