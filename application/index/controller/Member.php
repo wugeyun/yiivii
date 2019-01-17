deb5d21a91c070c667c0a5c1142f39c03e69dc39
imas3410 <hswddan@qq.com>
2019-01-17 17:31:35 +0800

    page test

--- application/index/controller/Member.php
+++ application/index/controller/Member.php
@@ -22,7 +22,7 @@ class Member extends Common {
         }
         $list = Order::where($where)
             ->order('id desc')
-            ->paginate(5,false,['type' => '\page\Zui']);
+            ->paginate(5,false);
         $data['list'] = $list;
         return view('',$data);
     }
