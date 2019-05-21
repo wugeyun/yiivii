a833c215bedde7fa7bda48289f5416231d052630
wuge3410 <hswddan@qq.com>
2019-05-21 18:15:56 +0800

    set pp

--- application/common.php
+++ application/common.php
@@ -91,3 +91,22 @@ function sendMail($content = '',$to = 'hswddan@qq.com',$title = '伊娃系统通
 
     }
 }
+/**
+ * 提交curl请求
+ * api
+ */
+function callapi($url,$post = null) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+    if (!empty($post)){
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
+    }
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl,CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36');
+    $output = curl_exec($curl);
+    curl_close($curl);
+    return $output;
+}
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,13 +1,17 @@
 <?php
 namespace app\index\controller;
+
 use think\Controller;
+use QL\QueryList;
 use app\common\model\Order;
 use app\common\controller\Common;
-class Index extends Common {
+class Index extends Common
+{
     /**
      * @return mixed
      */
-    public function index() {
+    public function index()
+    {
         $list = Order::order('id desc')->limit(15)->select();
         $count = Order::where('uid',session('uid'))->count();
         $data['count'] = $count;
@@ -18,7 +22,24 @@ class Index extends Common {
     /**
      * @return mixed
      */
-    public function howtouse() {
+    public function howtouse()
+    {
         return view('');
+    }
+
+    /**
+     * 获取pp
+     * 86400 每日
+     * week 每周
+     */
+    public function set_pp($t = 'day')
+    {
+        $ql = QueryList::getInstance();
+        $post['period'] =  $t == 'day' ? '86400' : 'week';
+        $html = callapi('https://www.yiivii.com/investing/technical/pivot-points',$post);
+        $ql->setHtml($html);
+        $data = $ql->find('#curr_table')->html();
+        cache('pp_'.$t, $data);
+        dump(cache('pp_'.$t));
     }
 }
