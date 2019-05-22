dd21cdb27665e2f9ca9ada5b9be1a2efb0c1fc91
wuge3410 <hswddan@qq.com>
2019-05-22 12:56:11 +0800

    baike

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,6 +2,7 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Cache;
 use QL\QueryList;
 use app\common\model\Order;
 use app\common\controller\Common;
@@ -36,7 +37,24 @@ class Index extends Common
      */
     public function pp($t)
     {
-        $data['pp'] = cache('pp_'.$t) ?: '数据加载中...';
+        $data['pp'] = Cache::get('pp_'.$t) ?: '数据加载中...';
+        return view('',$data);
+    }
+
+    /**
+     * 科普 枢轴点
+     * main-content
+     */
+    public function baike()
+    {
+        $url = 'https://baike.baidu.com/item/%E6%9E%A2%E8%BD%B4%E7%82%B9/6689552';
+        //写入缓存，时间3600
+        Cache::remember('baike',function() use ($url){
+            $ql = QueryList::get($url);
+            return $ql->find('.main-content')->html();
+        },3600);
+        $data['url'] = $url;
+        $data['baike'] = Cache::get('baike') ?: '数据加载中...';
         return view('',$data);
     }
 
@@ -52,7 +70,7 @@ class Index extends Common
         $html = callapi('https://www.yiivii.com/investing/technical/pivot-points',$post);
         $ql->setHtml($html);
         $data = $ql->find('#curr_table')->html();
-        cache('pp_'.$t, $data);
+        Cache::set('pp_'.$t, $data);
         echo 'set pp ok ';
         echo date('Y-m-d H:i:s');
     }
