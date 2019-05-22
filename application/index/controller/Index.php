2502b89894cd0c7ff2e2bbef076aacf138e7e447
wuge3410 <hswddan@qq.com>
2019-05-22 14:36:19 +0800

    debug open

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -57,6 +57,7 @@ class Index extends Common
         },3600);
         $data['url'] = $url;
         $data['baike'] = Cache::get('baike') ?: '数据加载中...';
+        dump($data['baike']['css']);
         return view('',$data);
     }
 
